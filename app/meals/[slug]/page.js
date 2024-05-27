9311b00ceca130e9d570698be03a29d37739310c
mitso-ksr <mitsonary@gmail.com>
2024-05-27 22:10:34 +0330

    adding dynamic metadata to pages

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -2,6 +2,17 @@ import Image from "next/image";
 import classes from "./page.module.css";
 import { getMeal } from "@/lib/meals";
 import { notFound } from "next/navigation";
+
+export async function generateMetadata({ params }) {
+  const meal = getMeal(params.slug);
+  if (!meal) {
+    notFound();
+  }
+  return {
+    title: meal.title,
+    description: meal.summary,
+  };
+}
 export default async function MealDetailsPage({ params }) {
   const meal = getMeal(params.slug);
   if (!meal) {
