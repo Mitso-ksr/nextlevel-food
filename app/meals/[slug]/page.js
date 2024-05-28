b1ccfba95dac37e30da562e19429947a8d8f642a
mitso-ksr <mitsonary@gmail.com>
2024-05-28 14:05:15 +0330

    adding liara data server for fetching images

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -23,7 +23,11 @@ export default async function MealDetailsPage({ params }) {
     <>
       <header className={classes.header}>
         <div className={classes.image}>
-          <Image src={meal.image} alt={meal.title} fill />
+          <Image
+            src={`https://nextlevelfood.storage.iran.liara.space/${meal.image}`}
+            alt={meal.title}
+            fill
+          />
         </div>
         <div className={classes.headerText}>
           <h1>{meal.title}</h1>
